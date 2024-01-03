c74e1f805faac2d0b9694c82c19266f249cb8a59
Gianluca Toffanin <gianluca.toffanin@hibo.it>
2024-01-03 16:45:11 +0100

    tesseramento, img data entry

--- app/tesseramento/page.js
+++ app/tesseramento/page.js
@@ -2,6 +2,45 @@ export default function Tesseramento() {
 
  return (
   <>
+   <div className="center text-center text-xl leading-loose p-24 bg-white">
+    <div className="relative">
+     <p>
+      Per qualsiasi informazione sulle iscrizioni potete rivolgervi alla <b>segreteria della scuderia.</b>
+     </p>
+
+     <p>
+      Vi forniremo tutta la <b>documentazione</b> e le <b>informazioni</b> necessarie per diventare <b>soci.</b>
+     </p>
+
+     <p>E' inoltre possibile richiedere una <b>iscrizione on-line</b> utilizzando le schede di iscrizione<br></br>
+
+      presenti <b>qui sotto</b> che si prega di <b>stampare, compilare</b> ed <b>inviarci via email.</b></p>
+
+     <p>
+      Le tessere e tutto il materiale previsto per l'anno in corso saranno inviati al <b>domicilio indicato.</b>
+     </p>
+    </div>
+    <div className="relative">
+     <div className="text-4xl my-8">
+      TESSERAMENTI 2024
+     </div>
+     Quota socio ordinario: <b>45€</b><br></br>
+
+     - Il rinnovo per tutti i soci dell'anno precedente è scontato e pari a <b>35€**</b><br></br>
+
+     Quota socio sostenitore: <b>25€</b>
+     <a href="https://drive.google.com/open?id=1UcAcoGPkt4lE3akDlI6NzzH1IyFY7Pq4" className="uppercase text-4xl my-8 underline block">
+      scheda iscrizione
+     </a>
+
+     <b>- Coordinate bancarie -</b><br></br>
+
+     Beneficiario: <b>Scuderia Legend Cars</b><br></br>
+
+     IBAN: <b>IT98Y0306967684510749157221</b>
+    </div>
+
+   </div>
   </>
  )
 }
